ad8e8b27d80aec555a4159b271da943c092aa876
Kimi Li <kimi@autoalign.ai>
2025-01-16 16:43:30 -0500

    feat: remove ngram token normalizer because index can't handle one to many normalizers yet

--- tests/test_normalizers.py
+++ tests/test_normalizers.py
@@ -1,7 +1,6 @@
 from src.textsearchpy.normalizers import (
     LowerCaseNormalizer,
     StopwordsNormalizer,
-    NGramNormalizer,
 )
 
 
@@ -33,61 +32,3 @@ def test_stopwrods_normalizer_custom_words():
     tokens = ["this", "is", "mock", "testing"]
     t_tokens = normalizer.normalize(tokens)
     assert t_tokens == ["this", "is", "testing"]
-
-
-def test_ngram_normalizer():
-    normalizer = NGramNormalizer(min_gram=3, max_gram=5, preserve_original=False)
-
-    tokens = ["this", "is", "testing"]
-    t_tokens = normalizer.normalize(tokens)
-
-    assert t_tokens == [
-        "thi",
-        "this",
-        "his",
-        "is",
-        "tes",
-        "test",
-        "testi",
-        "est",
-        "esti",
-        "estin",
-        "sti",
-        "stin",
-        "sting",
-        "tin",
-        "ting",
-        "ing",
-    ]
-
-
-def test_ngram_normalizer_preserve_original():
-    normalizer = NGramNormalizer(min_gram=2, max_gram=3, preserve_original=True)
-    tokens = ["this", "works"]
-    t_tokens = normalizer.normalize(tokens)
-
-    assert t_tokens == [
-        "this",
-        "th",
-        "thi",
-        "hi",
-        "his",
-        "is",
-        "works",
-        "wo",
-        "wor",
-        "or",
-        "ork",
-        "rk",
-        "rks",
-        "ks",
-    ]
-
-
-def test_ngram_normalizer_same_min_max():
-    normalizer = NGramNormalizer(min_gram=4, max_gram=4, preserve_original=False)
-
-    tokens = ["this", "is", "works"]
-    t_tokens = normalizer.normalize(tokens)
-
-    assert t_tokens == ["this", "is", "work", "orks"]
