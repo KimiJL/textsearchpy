a0042388543709154b067f4e87adf2752a1847f1
Kimi Li <kimi@autoalign.ai>
2025-01-15 22:12:08 -0500

    feat: add n gram normalizers

--- src/textsearchpy/normalizers.py
+++ src/textsearchpy/normalizers.py
@@ -4,12 +4,12 @@ from typing import List
 
 class TokenNormalizer(ABC):
     @abstractmethod
-    def normalize(self, tokens: List[str]):
+    def normalize(self, tokens: List[str]) -> List[str]:
         pass
 
 
 class LowerCaseNormalizer(TokenNormalizer):
-    def normalize(self, tokens: List[str]):
+    def normalize(self, tokens: List[str]) -> List[str]:
         t_tokens = [t.lower() for t in tokens]
         return t_tokens
 
@@ -27,10 +27,51 @@ class StopwordsNormalizer(TokenNormalizer):
         else:
             self.stopwords = set(DEFAULT_STOP_WORDS)
 
-    def normalize(self, tokens: List[str]):
+    def normalize(self, tokens: List[str]) -> List[str]:
         if not self.stopwords:
             return tokens
         t_tokens = [t for t in tokens if t not in self.stopwords]
+        return t_tokens
+
+
+class NGramNormalizer(TokenNormalizer):
+    """
+    Normalizer that for given input token, converts it to a list of possible n grams from min_gram to max_gram
+
+    min_gram: int - minimum length of n grams to generate
+    max_gram: int - maximum length of n grams to generate
+    preserve_original: bool - set to true if original token should be included in the final tokens output
+    """
+
+    def __init__(self, min_gram: int, max_gram: int, preserve_original: bool):
+        super().__init__()
+        self.min_gram = min_gram
+        self.max_gram = max_gram
+        self.preserve_original = preserve_original
+
+    def _convert_token_to_ngrams(self, token: str):
+        # only possibility when token is smaller than min gram len
+        if len(token) <= self.min_gram:
+            return [token]
+
+        ngrams = []
+        if self.preserve_original:
+            ngrams.append(token)
+
+        for i in range(len(token)):
+            # max_gram + 1 because range is not inclusive to last number
+            for j in range(self.min_gram, self.max_gram + 1):
+                if i + j > len(token):
+                    break
+                ngrams.append(token[i : i + j])
+
+        return ngrams
+
+    def normalize(self, tokens) -> List[str]:
+        t_tokens = []
+        for t in tokens:
+            t_tokens.extend(self._convert_token_to_ngrams(t))
+
         return t_tokens
 
 
